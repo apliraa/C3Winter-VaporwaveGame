e2440418c77e67322e14ae6960ecbc60f918066e
iianTP <158515401+iianTP@users.noreply.github.com>
2026-07-24 20:52:22 -0300

    feat: arrastar janelas

--- scripts/NewWindow.cs
+++ scripts/NewWindow.cs
@@ -1,20 +1,41 @@
 using Godot;
 using System;
 
-public partial class NewWindow : CanvasLayer
+public partial class NewWindow : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private Vector2 initMousePosit;
+	private Vector2 initWindowPosit;
+
+	private bool draging;
+
 	public override void _Process(double delta)
 	{
+		if (draging)
+			GlobalPosition = initWindowPosit + GetGlobalMousePosition() - initMousePosit;
+		
 	}
 	public void _on_close_button_pressed(){
 		CallDeferred("free");
 		
+	}
+
+	public void _on_bar_button_down()
+	{
+		draging = true;
+	
+		if (initMousePosit == Vector2.Zero)
+		{
+			initMousePosit = GetGlobalMousePosition();
+			initWindowPosit = GlobalPosition;
+		}
+
+	}
+
+	public void _on_bar_button_up()
+	{
+		draging = false;
+		initMousePosit = Vector2.Zero;
 	}
 	
 }
