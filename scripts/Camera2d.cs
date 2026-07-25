5cb7d166d66c8164cdb2c3d44679a62ccd4aa5fe
tcleimig <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>
2026-07-25 16:07:42 -0300

    texto do celular funcional

--- scripts/Camera2d.cs
+++ scripts/Camera2d.cs
@@ -4,34 +4,65 @@ using System.Collections.Generic;
 
 public partial class Camera2d : Camera2D
 {
-	private List<string> Respostas = new List<string> { "Ailton", "Faca", "Esfaqueado", "10:40", "Bunker" };
+	private List<string> Answears = new List<string> { "Ailton", "Faca", "Esfaqueado", "10:40", "Bunker" };
 
-	private List<string> palavrasAcertadas = new List<string>();
-	
-	private void OnLineEditTextSubmitted(string newText)
+	private List<string> Dialogue = new List<string>
 	{
-		bool acertouNovaPalavra = false;
-		
-		for (int i = 0; i < Respostas.Count; i++)
+		"Qual o nome do assassino?",
+		"Qual a arma utilizada no crime?",
+		"Qual a causa da morte?",
+		"Qual o horário do assassinato? (hh:mm):",
+		"Onde o corpo foi encontrado?",
+	
+	};
+
+	private List<string> wordsRight = new List<string>();
+
+	private RichTextLabel labelDialogue;
+
+	public override void _Ready()
+	{
+		labelDialogue = GetNode<RichTextLabel>("Window/Panel/Label");
+
+		if(labelDialogue == null)
 		{
-			string ordem = Respostas[i];
-			
-			if (newText.Equals(ordem, StringComparison.OrdinalIgnoreCase))
-			{
-				if (!palavrasAcertadas.Contains(ordem))
-				{
-					palavrasAcertadas.Add(ordem);
-					acertouNovaPalavra = true;
-					break;
-				}
-			}
+			GD.PrintErr("Erro: O nó 'Window/Dialogue' não foi encontrado.");
 		}
 
-		if (acertouNovaPalavra)
+		else
 		{
-			GD.Print($"{palavrasAcertadas.Count}/5.");
+			
+		}
 
-			if (palavrasAcertadas.Count == 5)
+		UpdateDialogue();
+	}
+	
+
+	private void OnLineEditTextSubmitted(string newText)
+	{
+		
+		if (wordsRight.Count >= Answears.Count)
+		{
+			return;
+		}
+		
+		bool gotWordRight = false;
+		
+		int currentIndex = wordsRight.Count;
+
+		string expectedAnswer = Answears[currentIndex];
+
+		if (newText.Equals(expectedAnswer, StringComparison.OrdinalIgnoreCase))
+		{
+			wordsRight.Add(expectedAnswer);
+			gotWordRight = true;
+		}		
+
+		if (gotWordRight)
+		{
+			GD.Print($"{wordsRight.Count}/5.");
+
+			if (wordsRight.Count == 5)
 			{
 				GD.Print("correto");
 				GetNode<Window>("Window").Hide();
@@ -39,13 +70,25 @@ public partial class Camera2d : Camera2D
 		}
 		else
 		{
-			
+			wordsRight.Clear();
 		}
 
 		var campoTexto = GetNodeOrNull<LineEdit>("Window/LineEdit");
 		if (campoTexto != null)
 		{
 			campoTexto.Text = "";
+		}
+
+		UpdateDialogue();
+
+	}
+
+	private void UpdateDialogue()
+	{
+		if (labelDialogue != null && wordsRight.Count < Dialogue.Count)
+		{
+			labelDialogue.Text = Dialogue[wordsRight.Count];
+			labelDialogue.Visible = true;
 		}
 	}
 	
