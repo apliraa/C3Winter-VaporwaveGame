195f756715609d4c895b8e661f8d7608446a2b02
apliraa <arthur.p.lira@gmail.com>
2026-07-25 13:56:39 -0300

    fix: ajuste no grid dos apps para fazer o reinicio do PC

--- scripts/Root.cs
+++ scripts/Root.cs
@@ -3,14 +3,13 @@ using System;
 
 public partial class Root : Control
 {
+	//logica de reinicio do pc
 	
 	
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
